9931e166822f92630eb6ea1c4980ed0ea70791f7
Genci <gr29911@seeu.edu.mk>
2024-10-28 23:17:06 +0100

    C

--- task_tracker_backend/app.py
+++ task_tracker_backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from extensions import db
+from extensions import db,cors
 from flask_migrate import Migrate
 from flask_restful import Api
 from routes import init_routes 
@@ -8,11 +8,13 @@ from schemas import UserSchema
 app = Flask(__name__)
 app.config.from_object('config.Config')
 app.config['JWT_SECRET_KEY'] = 'sdkfjhdsi3839!nkjdf4nNNNdn' 
+##app.cli.add_command(manage.create_user)
+
 db.init_app(app)
 migrate = Migrate(app, db)
-
-# cors.init_app(app,resources={r"/api/*":{"origins":["http//localhost"]}})
+cors.init_app(app,resources={r"/api/*":{"origins":["http//localhost"]}})
 # jwt.init_app(app)
+
 api = Api(app)
 init_routes(api)
 
--- task_tracker_backend/extensions.py
+++ task_tracker_backend/extensions.py
@@ -1,10 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
 from passlib.context import CryptContext
 from flask_jwt_extended import JWTManager
 from  flask_marshmallow import Marshmallow
 db = SQLAlchemy()
 ma=Marshmallow()
-
+cors=CORS()
 pwd_context=CryptContext(schemes=["pbkdf2_sha256"],deprecated="auto")
 # jwt=JWTManager()
